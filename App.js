dd4c7778922b9daf7e06adcf635991cd108ab731
jack <jackzhen999@gmail.com>
2020-12-23 11:18:23 -0800

    set up Flask backend

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [time,setTime] = useState(0)
+
+  useEffect(() => {
+    fetch('http://192.168.86.59:5000/').then(res => res.json()).then(data => {
+      setTime(data.time);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>The time is : {time}</Text>
       <StatusBar style="auto" />
     </View>
   );
