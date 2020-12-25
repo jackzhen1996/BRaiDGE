3a43f6ee55771fd336e825abd074ff667bed7627
jack <jackzhen999@gmail.com>
2020-12-24 20:54:37 -0800

    Added test JSON of 100 objects and track long,lat deltas

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState,} from 'react';
 import {Dimensions,Button, StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
-  const [time,setTime] = useState(0);
   const [showMap,setMap] = useState(false);
+  const [longDelta,setLongDelta] = useState(0);
+  const [latDelta,setLatDelta] = useState(0);
+
+  //test data: about 100 rows 
+  var data = require('./data/new_data_latlong_JSON.json');
 
   //useEffect(() => {
-  //  //On PC, replace IP with address with IPv4
-  //  //Format :'IP'/'MY_API'
-  //  fetch('http://192.168.86.59:5000/time').then(res => res.json()).then(data => {
-  //    setTime(data.time);
-  //  });
+    //On PC, replace IP with address with IPv4
+    //Format :'IP'/'MY_API'
+    //fetch('http://192.168.86.59:5000/time').then(res => res.json()).then(data => {
+    //  setTime(data.time);
+    //});
   //}, []);
 
   return (
@@ -19,12 +23,32 @@ export default function App() {
        <MapView
           style = {styles.map}
           initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          //Location of Golden Gate Bridge
+          latitude: 37.8197222,
+          longitude: -122.4788889,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
           }}
-        />
+
+          //onPress = {e=>console.log("Pressed:" , e.nativeEvent)}
+          onRegionChange = {e=>{
+            setLongDelta(e.longitudeDelta);
+            setLatDelta(e.latitudeDelta);
+          }}
+          //onRegionChangeComplete = {e=>console.log('Region changed', e.nativeEvent)}
+        
+        >
+        {data.map((point,index)=>(
+          <Marker 
+            coordinate = {{latitude:point.lat_deg, longitude:point.long_deg}}
+            title = {'' + index}
+          />
+        ))}
+        </MapView>
+        <Text style = {{fontSize: 20,position:"absolute", bottom:50}}>
+          LongDelta: {longDelta.toFixed(4)}  , LatDelta: {latDelta.toFixed(4)} {'\n'}
+          X-miles : {longDelta.toFixed(4)*69}
+        </Text>
         </View>
   )
   }
